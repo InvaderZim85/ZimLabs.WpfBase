b255a9c9bca32163761c28dccd293a88720e8717
Andreas Pouwels <apouwels85@gmail.com>
2021-12-12 15:46:02 +0100

    Initial commit

---
+++ ZimLabs.WpfBase/ObservableObject.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace ZimLabs.WpfBase
+{
+    /// <summary>
+    /// This class allows simpler handling of the INotifyPropertyChanged
+    /// </summary>
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// The different property access types
+        /// </summary>
+        [Flags]
+        public enum PropertyAccessType
+        {
+            /// <summary>
+            /// All properties
+            /// </summary>
+            Any = 0,
+            /// <summary>
+            /// Only properties which are readable
+            /// </summary>
+            Read = 1,
+            /// <summary>
+            /// Only properties which are write able
+            /// </summary>
+            Write = 2
+        }
+
+        /// <summary>
+        /// </summary>
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        /// <summary>
+        /// Raises the OnPropertyChanged event
+        /// </summary>
+        /// <param name="propertyName">Name of the property (if none is given CallerMemberName will be used)</param>
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Raises the OnPropertyChanged event for a list of properties
+        /// </summary>
+        /// <param name="properties">The list of properties</param>
+        protected virtual void OnPropertyChanged(IEnumerable<string> properties)
+        {
+            foreach (var property in properties)
+            {
+                OnPropertyChanged(property);
+            }
+        }
+
+        /// <summary>
+        /// Sets a field to the given value
+        /// </summary>
+        /// <typeparam name="T">Type of the value</typeparam>
+        /// <param name="field">Reference of the given field</param>
+        /// <param name="value">Value which will be set to the field</param>
+        /// <param name="propertyName">Name of the property (if none is given CallerMemberName will be used)</param>
+        /// <returns>True if the value has changed or false if it is equal</returns>
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            // ReSharper disable once ExplicitCallerInfoArgument
+            // Don't remove propertyName! CallerMemberName would be SetField and not the property!
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        /// <summary>
+        /// Sets a field of the given class to the given value
+        /// </summary>
+        /// <typeparam name="T">The type of the class</typeparam>
+        /// <param name="parent">The class which contains the field</param>
+        /// <param name="property">The name of the field</param>
+        /// <param name="value">The new value</param>
+        /// <param name="propertyName">Name of the property (if none is given CallerMemberName will be used)</param>
+        /// <returns>True if the value has changed or false if it is equal</returns>
+        protected bool SetField<T>(ref T parent, string property, object value,
+            [CallerMemberName] string? propertyName = null)
+        {
+            if (parent == null)
+                return false;
+
+            if (string.IsNullOrEmpty(property))
+                throw new ArgumentNullException(nameof(property));
+
+            var propertyInfo = parent.GetType().GetProperty(property);
+            if (propertyInfo == null)
+                throw new NullReferenceException($"The property with the name \"{property}\" does not exist.");
+
+            var currentValue = propertyInfo.GetValue(parent, null);
+            if (currentValue?.Equals(value) ?? false)
+                return false;
+
+            propertyInfo.SetValue(parent, Convert.ChangeType(value, propertyInfo.PropertyType), null);
+            OnPropertyChanged(propertyName);
+
+            return true;
+        }
+
+        /// <summary>
+        /// Gets all properties of the deriving class
+        /// </summary>
+        /// <param name="accessType">The <see cref="PropertyAccessType"/> of the properties (optional, default = Read | Write)</param>
+        /// <param name="onlyPublic"><see langword="true"/> to get only public properties, otherwise <see langword="false"/> (optional, default = <see langword="true"/></param>
+        /// <returns></returns>
+        protected List<string> GetProperties(PropertyAccessType accessType = PropertyAccessType.Read | PropertyAccessType.Write, bool onlyPublic = true)
+        {
+            if (!GetType().IsClass)
+                return new List<string>();
+
+            var properties = (int)accessType switch
+            {
+                1 => GetType().GetProperties().Where(w => w.CanRead).ToList(),
+                2 => GetType().GetProperties().Where(w => w.CanWrite).ToList(),
+                3 => GetType().GetProperties().Where(w => w.CanRead && w.CanWrite).ToList(),
+                _ => GetType().GetProperties().ToList()
+            };
+
+            return onlyPublic
+                ? properties.Where(w => w.GetMethod?.IsPublic ?? false).Select(s => s.Name).ToList()
+                : properties.Select(s => s.Name).ToList();
+        }
+    }
+}
---
+++ ZimLabs.WpfBase/RelayCommand.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace ZimLabs.WpfBase
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> _action;
+        private readonly Predicate<T> _canExecute;
+
+        /// <inheritdoc />
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:WpfUtility.Services.RelayCommand`1" /> class.
+        /// </summary>
+        /// <param name="action">The execute method.</param>
+        public RelayCommand(Action<T> action)
+            : this(action, null)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RelayCommand{T}" /> class.
+        /// </summary>
+        /// <param name="action">The execute method.</param>
+        /// <param name="canExecute">The can execute method.</param>
+        public RelayCommand(Action<T> action, Predicate<T> canExecute)
+        {
+            _action = action ?? throw new ArgumentNullException(nameof(action));
+            _canExecute = canExecute;
+        }
+
+        /// <summary>
+        /// Defines the method that determines whether the command can action in its current state.
+        /// </summary>
+        /// <param name="parameter">
+        /// Data used by the command.  If the command does not require data to be passed, this object can
+        /// be set to null.
+        /// </param>
+        /// <returns><c>true</c> if this command can be executed; otherwise, <c>false</c>.</returns>
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute?.Invoke((T)parameter) ?? true;
+        }
+
+        /// <summary>
+        /// Occurs when changes occur that affect whether or not the command should action.
+        /// </summary>
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        /// <summary>
+        /// Executes the action
+        /// </summary>
+        /// <param name="parameter"></param>
+        public void Execute(object parameter)
+        {
+            _action((T)parameter);
+        }
+    }
+}
